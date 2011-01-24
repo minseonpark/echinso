934594f5f822b0edbc990518fc29f9349798a97d
Your name <youremail@example.com>
2011-01-24 13:52:01 +0900

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,6 +7,12 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => "ED's friends introduce | Home")
+    end
   end
 
   describe "GET 'contact'" do
@@ -14,6 +20,26 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                        :content =>
+                          "ED's friends introduce | Contact")
+    end
   end
-
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                        :content =>
+                          "ED's friends introduce | About")
+    end
+  end
 end
