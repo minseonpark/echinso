8daa85f0d490a563e9088af476963f38b326c82e
Your name <youremail@example.com>
2011-01-24 11:59:39 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Echinso::Application.config.secret_token = 'f2e9ed861a0bcc37d1b896b7887f594b56e9a84a7992f77cf6fac114f1d6da86e494f17a90892dad3da82010a364df963b64f5a0700183c09384e64c9ddd5a86'
