c44100b7f6e914d53b1cec98ec164c38651dea0d
Your name <youremail@example.com>
2011-01-25 11:17:35 +0900

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,17 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-    
+
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                        :content => "ED's friends introduce | Home")
+                        :content => "Echinso | Home")
     end
   end
 
@@ -20,26 +21,26 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
-    
+
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
                         :content =>
-                          "ED's friends introduce | Contact")
+                          "Echinso | Contact")
     end
   end
-  
+
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
     end
-    
+
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
                         :content =>
-                          "ED's friends introduce | About")
+                          "Echinso | About")
     end
   end
 end
